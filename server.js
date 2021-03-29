05fe2b25450335c0cd38147de2474a6c1db2f1a4
0994486@gmail.com <0994486@gmail.com>
2021-03-29 17:03:43 +0300

    adds server config

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require(`express`);
-const PORT = process.env.PORT || 5000;
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 
-express()
-  .get(`/`, (req, res) => {
-    res.append(`X-Author`, `rukivbruki`);
-    res.append(`Content-Type`, `application/javascript; charset=UTF-8`);
-    res.append(`Access-Control-Allow-Origin`, [`*`]);
-    res.send(`rukivbruki`);
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.svg'));
+
+//здесь наше приложение отдаёт статику
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+//простой тест сервера
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+//обслуживание html
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
