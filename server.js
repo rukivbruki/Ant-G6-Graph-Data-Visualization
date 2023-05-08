8ca27d1087636d81b00b9c82bc2ec5aeb762f78b
rukivbruki <0994486@gmail.com>
2023-05-08 04:55:44 +0300

    feat: adds some changes

--- server.js
+++ server.js
@@ -3,20 +3,16 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 
-// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.svg'));
 
-//здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-//простой тест сервера
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 
-//обслуживание html
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
